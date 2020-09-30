bb32c13b191d9409cf74988bb372f4e79f98bb9c
Johnny Austen <65448382+jmaletto1@users.noreply.github.com>
2020-09-30 10:10:09 +0100

    More updates

--- js/script.js
+++ js/script.js
@@ -3,88 +3,73 @@
 
 // Create the `showPage` function
 // This function will create and insert/append the elements needed to display a "page" of nine students
+let pageNo = 1;
 
-function searchData() {
+function searchForm() {
 const header = document.querySelector('.header');
-const label = document.createElement('label');
-label.for = "search";
-label.className = "student-search";
-header.appendChild(label);
-const input = document.createElement('input');
-input.id = "search";
-input.placeholder = "search by name..";
-header.appendChild(input);
-const searchButton = document.createElement('button');
-searchButton.type = 'button';
-searchButton.id = 'search';
-header.appendChild(searchButton);
-buttonIMG = document.createElement('img');
-buttonIMG.src = "img/icn-search.svg";
-searchButton.appendChild(buttonIMG);
-
-searchButton.addEventListener('submit', (e) => {
+let searchHTML = `<label for="search" class="student-search">
+            <input id="search" onkeyup="showPage(data, pageNo)" placeholder="Search by name...">
+            <button type="button" id="search"><img src="img/icn-search.svg" alt="Search icon"></button>
+          </label>`;
+header.innerHTML += searchHTML;
+header.addEventListener('submit', (e) => {
 	e.preventDefault();
 	alert(input.value);
 });          
 };
 
-searchData();
+searchForm();
 
 function showPage(list, page) {
+let input = document.getElementById('search');
+let nameKey = input.value;
+console.log(nameKey);
 let startIndex = (page * 9) - 9;
 let endIndex = (page * 9) - 1;
 const ul = document.querySelector('.student-list');
 ul.innerHTML = "";
 
 for (i=0; i<list.length; i++) {
-	if (i >= startIndex && i<= endIndex) {
-		const li = document.createElement('li');
-		li.className = "student-item cf";
-		ul.appendChild(li);
-		const div = document.createElement('div');
-		div.className = "student-details";
-		li.appendChild(div);
-		const img = document.createElement('img');
-		img.className = "avatar";
-		img.src = `${list[i].picture.thumbnail}`;
-		img.alt = "Profile Picture";
-		div.appendChild(img);
-		const h3 = document.createElement('h3');
-		h3.textContent = `${list[i].name.title} ${list[i].name.first} ${list[i].name.last}`;
-		div.appendChild(h3);
-		const span = document.createElement('span');
-		span.className = "email";
-		span.textContent = `${list[i].email}`;
-		div.appendChild(span);
-		// const div2 = document.createElement('div');
-		// div2.className = "joined-details";
-		// ul.appendChild(div2);
-		const span2 = document.createElement('span');
-		span2.className = "date";
-		span2.textContent = `${list[i].registered.date}`;
-		div.appendChild(span2);
-
-		// let html = `
-		// 	  <li class="student-item cf">
-  //         <div class="student-details">
-  //           <img class="avatar" src="${list[i].picture.thumbnail}" alt="Profile Picture">
-  //           <h3>${list[i].name.title} ${list[i].name.first} ${list[i].name.last}</h3>
-  //           <span class="email">${list[i].email}</span>
-  //         </div>
-  //         <div class="joined-details">
-  //           <span class="date">${list[i].registered.date}</span>
-  //         </div>
-  //       </li>
-		// `;
-		// ul.innerHTML += html;
-
-		} else {
-			// alert("Hmm!");
-	}
+	if (nameKey === '') {
+		if (i >= startIndex && i<= endIndex && nameKey === '') {
+			listMaker();
+			function listMaker() {
+			const li = document.createElement('li');
+			li.className = "student-item cf";
+			ul.appendChild(li);
+			const div = document.createElement('div');
+			div.className = "student-details";
+			li.appendChild(div);
+			const img = document.createElement('img');
+			img.className = "avatar";
+			img.src = `${list[i].picture.thumbnail}`;
+			img.alt = "Profile Picture";
+			div.appendChild(img);
+			const h3 = document.createElement('h3');
+			h3.textContent = `${list[i].name.title} ${list[i].name.first} ${list[i].name.last}`;
+			div.appendChild(h3);
+			const span = document.createElement('span');
+			span.className = "email";
+			span.textContent = `${list[i].email}`;
+			div.appendChild(span);
+			// const div2 = document.createElement('div');
+			// div2.className = "joined-details";
+			// ul.appendChild(div2);
+			const span2 = document.createElement('span');
+			span2.className = "date";
+			span2.textContent = `${list[i].registered.date}`;
+			div.appendChild(span2);
+	};
 }
+		} else if (nameKey) {
+						if (list[i].name.first === nameKey || list[i].name.last === nameKey) {
+								for (i=0; i<list.length; i++) {
+				console.log(list[i].name.first);
+listMaker(data);
+	}
+}}}
 };
 
-let pageNo = 1;
 showPage(data, pageNo);
 // pageNo = prompt("Which page number would you like to see?");
 // showPage(data, pageNo);
