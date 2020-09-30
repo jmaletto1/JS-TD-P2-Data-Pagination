4ec6c1371667a452916ba9e9c632e9eedb1fe8c7
Johnny Austen <65448382+jmaletto1@users.noreply.github.com>
2020-09-30 15:04:47 +0100

    First Draft Revisions

--- js/script.js
+++ js/script.js
@@ -14,6 +14,8 @@ ul.innerHTML = "";
 
 for (let i=0; i<list.length; i++) {
 		if (i >= startIndex && i<= endIndex) {
+// The function now creates the full UL element, step by step.
+// The relevant array/object data is displayed for each result.			
 			const li = document.createElement('li');
 			li.className = "student-item cf";
 			ul.appendChild(li);
@@ -46,22 +48,32 @@ for (let i=0; i<list.length; i++) {
 // */
 
 function appendPageLinks(list) {
+// Here I've set the constants for the Number of Pages, and assigned the lowerUL to the .link-list section.
 const noOfPages = list.length / 9;
 const lowerUL = document.querySelector('.link-list');
 lowerUL.innerHTML = '';
+
+// We then loop through the Number of Pages (noOfPages) to create the required amount of pagination buttons.
 for (pNum=1; pNum<noOfPages+1; pNum++) {
 	li = document.createElement('li');
 	button = document.createElement('button');
 	button.id = pNum;
 	button.textContent = pNum;
-	// button.className = "active";
+
+// If the button's textContent is equal to the page number, we set the className to "active". If not, it is set to an empty string.
 	if (button.textContent == pageNo) {
 		button.className = "active";
 	} else {
 		button.className = ""
 	}
+
+// These page buttons are then appended to index.hmtl	
 	lowerUL.appendChild(li);
 	li.appendChild(button);
+
+// We then listen out for a button click, which starts an arrow function. This selects all of the buttons in the li, and loops through them.
+// If any of the buttons have an "active" classname, it is removed. Outside of the if statement, the page that matches the targeted button
+// has it's className set to active.	
 	button.addEventListener('click', (e) => {
 const totalButtons = lowerUL.querySelectorAll('li button');
 for (let butNo = 0; butNo < totalButtons.length; butNo++) {
@@ -71,12 +83,14 @@ for (let butNo = 0; butNo < totalButtons.length; butNo++) {
 	e.target.className = "active";
 	pageNo = e.target.textContent;
 }
+// We then call the showPage function to show the correct set of results, from the relevant page number.
 showPage(list, pageNo);
 });
 };
 };
 
 // Search Function
+// This function starts off by selecting the header element, and adding the necessary HTML to accept search input.
 function searchForm() {
 const header = document.querySelector('.header');
 let searchHTML = `<label for="search" class="student-search">
@@ -84,18 +98,27 @@ let searchHTML = `<label for="search" class="student-search">
             <button type="button" id="searchButton"><img src="img/icn-search.svg" alt="Search icon"></button>
           </label>`;          
 header.innerHTML += searchHTML;
-const searchData = document.getElementById('searchMaster');
 
+// The function then selects the "form" input and listens for a 'keyup' event. Next, it convers the value submitted to upper case.
+const searchData = document.getElementById('searchMaster');
 searchData.addEventListener('keyup', (e) => {
 searchValue = e.target.value.toUpperCase();
+
+// Next the function creates an empty list. This will then loop through all the first and last name values, and assign them to variables.
 const results = [];
 for (let entry=0; entry<data.length; entry++) {
 	const firstName = data[entry].name.first.toUpperCase();
 	const lastName = data[entry].name.last.toUpperCase();
+
+// The function then compares the search term to both the first and last name values.	
 		if(firstName.includes(searchValue) || lastName.includes(searchValue)) {
+
+// If there is a match, the entry is appended to the results array. The showPage and appendPageLinks functions are then called.
 		results.push(data[entry])
 		showPage(results, 1);
 		appendPageLinks(results);
+
+// If there are no matches, the function prints a message to the UL, and does not display any page buttons.
 	} else if (results.length === 0) {
 		ul.innerHTML = "<div><h3> Unfortunately there are no matches! Please try again.</h3></div>";//
 		appendPageLinks(results);
